e1ff09bc311e0fed52d7a4f2a2faad7038e063fc
raihanhd12 <95725937+raihanhd12@users.noreply.github.com>
2023-03-31 09:19:35 +0700

    Delete WordCount.py

--- Pertemuan 5/WordCount.py
+++
@@ -1,8 +0,0 @@
-from operator import add
-lines = sc.textFile("file:///path/to/README.md")
-counts = lines.flatMap(lambda x: x.split(' ')) \
-              .map(lambda x: (x, 1)) \
-              .reduceByKey(add)
-output = counts.collect()
-for (word, count) in output:
-    print("%s: %i" % (word, count))
