05b541a8670c29c213f55abffbf562cdad8e3cc2
raihanhd12 <95725937+raihanhd12@users.noreply.github.com>
2023-04-02 17:08:20 +0700

    Code Chapter 3

--- Pertemuan 5/UnderstandingRDDs/UnderstandingRDDs.py
+++ Pertemuan 5/UnderstandingRDDs/UnderstandingRDDs.py
@@ -1,31 +1,19 @@
-'''
-Start Pyspark Command line using standalone master
-[root@myhost ~]# pyspark --master spark://sparkmasterhostname:7077 --total-executor-cores 4
-
-or
-
-[root@myhost ~]# pyspark --master local[4]
-
-'''
-
 # Check Default Parallelism
 sc.defaultParallelism
 
-
-#Let’s create a list, parallelize it and let’s check the number of partitions. 
+#Let's create a list, parallelize it and let's check the number of partitions. 
 myList = ["big", "data", "analytics", "hadoop" , "spark"]
 myRDD = sc.parallelize(myList)
 myRDD.getNumPartitions()
 
-#To override the default parallelism, provide specific number of partitions needed while creating the RDD. In this case let’s create the RDD with 6 partitions.
+#To override the default parallelism, provide specific number of partitions needed while creating the RDD. In this case let's create the RDD with 6 partitions.
 myRDDWithMorePartitions = sc.parallelize(myList,6)
 myRDDWithMorePartitions.getNumPartitions()
  
-#Let’s issue an action to count the number of elements in the list.
+#Let's issue an action to count the number of elements in the list.
 myRDD.count()
 
 #Display the data in each partition
-
 myRDD.mapPartitionsWithIndex(lambda index,iterator: ((index, list(iterator)),)).collect()
 
 #Increase number of partitions and display contents
@@ -38,4 +26,3 @@ myTwoPartitionsRDD.mapPartitionsWithIndex(lambda index,iterator: ((index, list(i
 
 # Check Lineage Graph
 print myTwoPartitionsRDD.toDebugString()
-
