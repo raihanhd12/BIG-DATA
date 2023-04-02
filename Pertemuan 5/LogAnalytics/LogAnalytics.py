1e61e7189a4d3dcd2c4346c41846519d35f64489
raihanhd12 <95725937+raihanhd12@users.noreply.github.com>
2023-04-02 16:28:33 +0700

    Code Chapter 3

--- Pertemuan 5/LogAnalytics/LogAnalytics.py
+++ Pertemuan 5/LogAnalytics/LogAnalytics.py
@@ -1,27 +1,19 @@
-import sys
-from operator import  add
-from pyspark import SparkContext
-
-# Check for number of inputs passed from command line
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print >> sys.stderr, "Usage: access_log.py <file>"
-        exit(-1)
-
-# Intialize the Spark Context with app name
-sc = SparkContext(appName="Log Analytics")
-
 # Get the lines from the textfile, create 4 partitions
-access_log = sc.textFile(sys.argv[1], 4)
+access_log = sc.textFile("file:/home/cloudera/spark-2.0.0-bin-hadoop2.7/pendidikan.txt", 4)
 
 #Filter Lines with ERROR only
-error_log = access_log.filter(lambda x: "ERROR" in x)
+error_log = access_log.filter(lambda x: "tinggi" in x)
 
 # Cache error log in memory
 cached_log = error_log.cache()
 
 # Now perform an action -  count
-print “Total number of error records are %s” % (cached_log.count())
+print "Total number of error records are %s" % (cached_log.count())
 
 # Now find the number of lines with 
-print “Number of product pages visited that have Errors is %s” % (cached_log.filter(lambda x: “product” in x).count()) 
+print "Number of product pages visited that have Errors is %s" % (cached_log.filter(lambda x: "pendidikan" in x).count())
+
+#Catatan
+# 1. buat file pendidikan.txt dan masukkan kata dengan cara : sudo vi pendidikan.txt
+# 2. cek status hadoop dengan cara : sudo service hadoop-hdfs-namenode status
+# 3. jika failed maka hidupkan dengan cara : sudo service hadoop-hdfs-namenode start
